d130e20a38faee54eeecc018e8e727cdeeb1bc4b
xemic <kan5016@konkuk.ac.kr>
2020-06-04 17:25:00 +0900

    init layout

--- app/src/main/java/com/example/undertrail/MainActivity.kt
+++ app/src/main/java/com/example/undertrail/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.undertrail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val quickUnderTrailFragment = QuickUnderTrailFragment()
+    val underTrailFragment = UnderTrailFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragmentInit()
+        navigatorInit()
+    }
+
+    private fun fragmentInit() {
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.addToBackStack(null)
+        fragment.replace(R.id.myFragment, quickUnderTrailFragment)
+        fragment.commit()
+    }
+
+    private fun navigatorInit() {
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.navigation_first -> {
+                    //fragment select
+                    val fragment = supportFragmentManager.beginTransaction()
+                    fragment.addToBackStack(null)
+                    fragment.replace(R.id.myFragment, quickUnderTrailFragment)
+                    fragment.commit()
+                    true
+                }
+                R.id.navigation_second -> {
+                    //fragment select
+                    val fragment = supportFragmentManager.beginTransaction()
+                    fragment.addToBackStack(null)
+                    fragment.replace(R.id.myFragment, underTrailFragment)
+                    fragment.commit()
+                    true
+                }
+                else -> { false }
+            }
+        }
     }
 }
